cc5035eedc5f55cab210f14d40a27c8ce86d8ab3
anilbidari <anil.bidari@thecloudenabled.com>
2021-09-16 16:30:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("/var/lib/jenkins/sc.json")
-  project     = "vocal-camera-264620"
+  credentials = file("/home/anil_singapore4/sc.json")
+  project     = "sg-project-1-264708"
   region      = "us-central1"
 }
